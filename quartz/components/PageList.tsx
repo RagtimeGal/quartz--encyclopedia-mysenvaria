c9580945b1a752a6f68078ccf928593108e6f2bf
Ragno <TerrariaOverlord11@gmail.com>
2025-04-18 03:01:12 -0400

    Update PageList.tsx

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -78,14 +78,14 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
               </p>
               <div class="desc">
               <h3>
-                  {isFolderPath(page.slug ?? "") ? (
-                    <span class="internal">{title}</span>
-                  ) : (
-                    <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
-                      {title}
-                    </a>
-                  )}
-                </h3>
+                {isFolderPath(page.slug ?? "") ? (
+                  <span class="internal folder-name">{title}</span>
+                ) : (
+                  <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
+                    {title}
+                  </a>
+                )}
+              </h3>
               </div>
               <ul class="tags">
                 {tags.map((tag) => (
