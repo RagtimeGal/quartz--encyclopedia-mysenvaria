6f674da8a2a0af19d4467ca19ab77d447f651b3b
Ragno <TerrariaOverlord11@gmail.com>
2025-04-18 02:28:40 -0400

    Update TagList.tsx

--- quartz/components/TagList.tsx
+++ quartz/components/TagList.tsx
@@ -1,22 +1,14 @@
-import { pathToRoot } from "../util/path"
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const TagList: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const tags = fileData.frontmatter?.tags
   if (tags && tags.length > 0) {
     return (
       <ul class={classNames(displayClass, "tags")}>
-        {tags.map((tag) => {
-          const display = `#${tag}`
-          // const linkDest = baseDir + `/tags/${slugTag(tag)}`  // Removed as it's no longer needed
-          return (
-            <li>
-              <span class="internal tag-link">
-                {display}
-              </span>
-            </li>
-          )
-        })}
+        {tags.map((tag) => (
+          <li>{tag}</li>
+        ))}
       </ul>
     )
   } else {
@@ -28,28 +20,14 @@ TagList.css = `
 .tags {
   list-style: none;
   display: flex;
-  padding-left: 0;
+  flex-wrap: wrap;
   gap: 0.4rem;
   margin: 1rem 0;
-  flex-wrap: wrap;
+  padding-left: 0;
 }
 
-.section-li > .section > .tags {
-  justify-content: flex-end;
-}
-  
 .tags > li {
-  display: inline-block;
   white-space: nowrap;
-  margin: 0;
-  overflow-wrap: normal;
-}
-
-span.internal.tag-link {
-  border-radius: 8px;
-  background-color: var(--highlight);
-  padding: 0.2rem 0.4rem;
-  margin: 0 0.1rem;
 }
 `
 
